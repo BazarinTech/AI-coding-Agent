0ee9d9ee097001bbc586a1cb75a44f51739c5dd0
Bazarin Ngigi <bazarintechnologies@gmail.com>
2025-09-18 16:26:17 +0300

    Feat: added gemini api

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+
 def main():
-    print("Hello from coding-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print('I need a prompt!')
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=prompt
+    )
+
+    print(response.text)
+
+    if response is None or response.usage_metadata is None:
+        print('Response is maliformed')
+        return
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
-if __name__ == "__main__":
-    main()
+main()
